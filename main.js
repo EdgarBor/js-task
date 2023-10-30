367f29bd1c6bf08fb3654c5757825205c214af9e
EdgarBor <boredgar1@gmail.com>
2023-10-30 16:06:05 +0400

    created task structure

---
+++ main.js
@@ -0,0 +1,16 @@
+let firstName = "Edqar";
+let surName = "Bor";
+let age = 28;
+let work = "HelpDesk specialist";
+let myBirth = "10.05.1995";
+
+
+document.write("FirstName: " + firstName);
+document.write('<br>');
+document.write("LastName: " + surName);
+document.write('<br>');
+document.write("Age: " + age);
+document.write('<br>');
+document.write("Work: " + work);
+document.write('<br>');
+document.write("Date Birth: " + myBirth);
